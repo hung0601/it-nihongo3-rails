fefb6d6b92fdc0b8d3ac8d6bbe77476f2da281fa
hung0601 <manhhung0601nd@gmail.com>
2023-01-29 22:46:33 +0700

    Add profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root to:'static_pages#home'
   devise_for :users
+  get 'users/show'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
