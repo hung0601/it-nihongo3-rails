b73fe208273e4230bfda741ffc0ca87fa42d3304
hung0601 <manhhung0601nd@gmail.com>
2023-01-31 15:15:51 +0700

    Add detail book

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'users/show'
   root to:'static_pages#home'
   devise_for :users
-  get 'users/show'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
